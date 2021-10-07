b9db41075758aa8c847782064ccf23a6e1600ab6
Faizan Ahmed <5ahmef94@solent.ac.uk>
2021-10-07 11:23:47 +0100

    Edited the old code and added new code to demonstrate the use of comparison operators.

--- basics/decisions/simple_decision/comparison_operators.py
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -1,36 +1,16 @@
-# This part  allows Beep to count which of the 3 numbers entered is even or odd
-print("Please enter the first whole number: ")
+# This code allows Beep to identify which of two numbers is smaller, bigger or equal to each other
+print("Please enter the first number:")
 first = int(input())
-print("Please enter the second whole number: ")
+print("Please enter the second number:")
 second = int(input())
-print("Please enter the third whole number: ")
-third = int(input())
-# These 2 variables below are used to check how many are odd/even
-odd_count = int()
-even_count = int()
 
-# The following 'if..elif..else' block is used to count how many are odd/even
-if first % 2 != 0:
-    odd_count += 1
-elif first % 2 == 0:
-    even_count += 1
+# This code checks which conditions are satisfied to output a relevant message
+if first < second:
+    print()
+    print("The first number is the smallest!")
+elif first > second:
+    print()
+    print("The second number is the smallest!")
 else:
     print()
-    print("Please input a whole number!")
-if second % 2 != 0:
-    odd_count += 1
-elif second % 2 == 0:
-    even_count += 1
-else:
-    print()
-    print("Please enter a whole number!")
-if third % 2 != 0:
-    odd_count += 1
-elif third % 2 == 0:
-    even_count += 1
-else:
-    print()
-    print("Please enter a whole number!")
-
-print()
-print(f"There were {even_count} even and {odd_count} odd numbers.")
+    print("Both are equal!")
