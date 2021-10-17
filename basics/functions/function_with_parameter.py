7ff8dde34a136927d5a481e3e21046f6400dd4e1
Faizan Ahmed <5ahmef94@solent.ac.uk>
2021-10-17 22:49:47 +0100

    Added code to demostrate the use of a user-defined function with a parameter.

---
+++ basics/functions/function_with_parameter.py
@@ -0,0 +1,19 @@
+# This program uses a function to help Beep and Bop escape, using a function.
+
+def escape_by(plan):
+    if plan == 'jumping over':
+        print("We cannot escape that way! The boulder is too big!")
+
+    elif plan == 'running around':
+        print("We cannot escape that way! The boulder is moving too fast!")
+
+    elif plan == 'going deeper':
+        print("That just might work! Let's go deeper!")
+
+    else:
+        print("We cannot escape that way! The boulder is in the way!")
+
+
+escape_by("jumping over")
+escape_by("running around")
+escape_by("going deeper")
