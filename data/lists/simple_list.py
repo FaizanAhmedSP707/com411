83cd2dcf89f16eae480975114c6b942aca78d975
Faizan Ahmed <5ahmef94@solent.ac.uk>
2021-10-28 11:20:25 +0100

    Added code to demonstrate a simple list.

---
+++ data/lists/simple_list.py
@@ -0,0 +1,15 @@
+# This program is to help Beep and Bop find their way out of the maze
+
+def directions():
+    # This function creates a list of directions for Beep and Bop to use
+    directions_list = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return directions_list
+
+
+def run():
+    # Calls the first function and prints the list
+    print(directions())
+
+
+if __name__ == '__main__':
+    run()
