8b8facfa3977583bf3310f69c5a16b75e3b4f87a
Faizan Ahmed <5ahmef94@solent.ac.uk>
2021-12-09 16:32:55 +0000

    Created a Planet class with 2 attributes that have empty lists assigned to them for the purpose of populating these lists with objects generated from the imported classes.

--- oop/planet.py
+++ oop/planet.py
@@ -9,35 +9,34 @@ from robot2 import Robot
 class Planet:
     # Initialise a dictionary containing a list of inhabitants for each key
     def __init__(self):
-        self.planet_inhabitants = {
-            'humans': [],
-            'robots': []
-        }
+        self.humans = []
+        self.robots = []
 
     def __repr__(self):
         # Return a string representation about the current object
-        return f"\nPlanet(humans={self.planet_inhabitants['humans']}, robots={self.planet_inhabitants['robots']})"
+        return f"\nPlanet(humans={self.humans}, robots={self.robots})"
 
     def __str__(self):
         # Return a readable string about the current object
-        return f"\nThis planet has {len(self.planet_inhabitants['humans'])} humans and " \
-               f"{len(self.planet_inhabitants['robots'])} robots."
+        return f"\nThis planet has {len(self.humans)} humans and {len(self.robots)} robots."
 
     def add_human(self, human):
-        # An object of the human class will be passed in as a parameter for adding to the dict
-        self.planet_inhabitants['humans'].append(human)
+        # An object of the human class will be passed in as a parameter for adding to the list of humans
+        self.humans.append(human)
 
     def remove_human(self, human):
-        # An object of the human class will be passed in as a parameter for removal from the dict
-        self.planet_inhabitants['humans'].remove(human)
+        # An object of the human class will be passed in as a parameter for removal from the humans list
+        pop_h_index = self.humans.index(human)
+        self.humans.pop(pop_h_index)
 
     def add_robot(self, robot):
-        # An object of the robot class will be passed in as a parameter for adding to the dict
-        self.planet_inhabitants['robots'].append(robot)
+        # An object of the robot class will be passed in as a parameter for adding to the list of robots
+        self.robots.append(robot)
 
     def remove_robot(self, robot):
-        # An object of the robot class will be passed in as a parameter for removal from the dict
-        self.planet_inhabitants['robots'].remove(robot)
+        # An object of the robot class will be passed in as a parameter for removal from the robots list
+        pop_r_index = self.robots.index(robot)
+        self.robots.pop(pop_r_index)
 
 
 if __name__ == '__main__':
@@ -55,6 +54,11 @@ if __name__ == '__main__':
     new_planet.add_human(Darren)
     beta = Robot("Beta")
     new_planet.add_robot(beta)
+    print(repr(new_planet))
+    Faizan = Human("Faizan")
+    new_planet.add_human(Faizan)
+    print(repr(new_planet))
+    new_planet.remove_human(Faizan)
     print(repr(new_planet))
 
     # Finally print out the planet object using the __str__ method
