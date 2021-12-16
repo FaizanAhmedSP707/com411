448f359a2265e6eaec5b7f6e98f4a2f625ca628a
Faizan Ahmed <5ahmef94@solent.ac.uk>
2021-12-16 12:54:07 +0000

    Renamed the files to avoid errors with other programs.

---
+++ oop/planet_21.py
@@ -0,0 +1,8 @@
+# I previously created the class Planet that allowed us to add humans and robots to a planet.
+# These were stored in a dictionary.  Now that I have the class Inhabitant, I no longer need a dictionary
+# or indeed separate methods for humans and robots.  This is because I can now store a single list of inhabitants.
+# We can always check whether an inhabitant is a human or a robot using the function isinstance.
+
+from human_2-1 import Human
+from robot_2-1 import Robot
+
